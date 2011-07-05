db69340f8ea7dc653e536b731c7298c4a8f1b3ae
vstoykov <vkstoykov@gmail.com>
2011-07-05 17:43:05 +0000

    fix time zone
    git-svn-id: https://venelins-site.googlecode.com/svn/django-site@51 f0a13019-6d36-bf16-1f23-85a5bddb9b10

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ MANAGERS = ADMINS
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Bulgaria/Sofia'
+TIME_ZONE = 'Europe/Sofia'
 DATETIME_FORMAT = "j F Y, H:i"
 
 # Language code for this installation. All choices can be found here:
