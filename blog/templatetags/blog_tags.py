f47e930979d05dbd6eae1c6c2ba90aaa529f679c
vstoykov <vkstoykov@gmail.com>
2012-03-25 11:28:59 +0300

    add template blog template app for render latest entries use new blog tag in home page template

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,21 @@
+from django.template import Library
+from blog.models import Entry
+
+register = Library()
+
+@register.inclusion_tag('blog/latest_entries.html', takes_context=True)
+def latest_entries(context, n):
+    """
+    Render latest n entries from blog
+    
+    Usage:
+    
+        {% latest_entries n %}
+    
+    """
+    latest = list(Entry.objects.order_by('-created')[:n+1])
+    context.update({
+        'latest': latest[:n],
+        'show_more_link': len(latest) == n+1
+    })
+    return context
