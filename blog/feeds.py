88d1370d64a7fd3d31e726cba8156c8839dc2c99
vstoykov <vkstoykov@gmail.com>
2012-03-26 19:36:15 +0300

    Blog RSS feed improvements

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
 from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords_html
 
 from blog.models import Category, Entry
 
 
-class LatestEntriesFeed(Feed):
+class EntriesFeed(Feed):
     title = u"Статии от блога на Венелин Стойков"
     link = "/blog/feed/"
-    description = u"Последните 10 статии от блога на Венелин Стойков"
+    description = u"Всички статии от блога на Венелин Стойков"
 
     def items(self):
-        return Entry.objects.order_by('-created')[:10]
+        return Entry.objects.select_related('category').order_by('-created')
 
     def item_title(self, item):
         return item.title
 
     def item_description(self, item):
-        return item.content
+        return truncatewords_html(item.content, 80)
+
+
+class LatestEntriesFeed(EntriesFeed):
+    description = u"Последните 10 статии от блога на Венелин Стойков"
+
+    def items(self):
+        return super(LatestEntriesFeed, self).items()[:10]
+
 
 latest_entries_feed = LatestEntriesFeed()
