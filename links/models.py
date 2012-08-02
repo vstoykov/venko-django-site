ad22b7ffb31c0391f9f0d8cf8b66d1eac05ac35e
Venelin Stoykov <vkstoykov@gmail.com>
2012-08-03 02:33:14 +0300

    Some polishing of the links models

--- links/models.py
+++ links/models.py
@@ -1,22 +1,34 @@
 from django.db import models
 
+
 class Category(models.Model):
     """
     Links Category
     """
     title = models.CharField(max_length=255)
     
-    def __unicode__(self):
-        return unicode(self.title)
-    
     class Meta:
-        verbose_name_plural = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __unicode__(self):
+        return self.title
 
 
 class Link(models.Model):
     """
     This model handle a links to the world
     """
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, related_name='links')
     title = models.CharField(max_length=255)
     url   = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Link'
+        verbose_name_plural = 'Links'
+        ordering = ('category',)
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.title, self.url)
+
+
