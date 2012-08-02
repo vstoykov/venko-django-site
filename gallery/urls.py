ece3984ba92cc31382030fe76e708f45598e2842
Venelin Stoykov <vkstoykov@gmail.com>
2012-08-03 00:53:57 +0300

    fix bug in gallery urls caused by moving gallery specific urls into gallery app

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gallery.views',
-    url(r'^gallery/$', 'view_galleries', name='galleries'),
-    url(r'^gallery/(?P<gallery>[\w\_\-]+)/$', 'view_galleries', name='gallery'),
+    url(r'^$', 'view_galleries', name='galleries'),
+    url(r'^(?P<gallery>[\w\_\-]+)/$', 'view_galleries', name='gallery'),
 )
