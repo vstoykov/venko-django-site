051928750f7809910f457ad2754893ac609a6dcb
Venelin Stoykov <vkstoykov@gmail.com>
2012-09-25 20:59:16 +0300

    Add a url pattern for home page from the flatpages

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('django.views.generic.simple',
-    url(r'^$', 'direct_to_template', kwargs={'template': 'home.html'}, name='home'),
+urlpatterns = patterns('django.contrib.flatpages.views',
+	url(r'^$', 'flatpage', kwargs={'url': '/'}, name='home'),
+) + patterns('django.views.generic.simple',
+    #url(r'^$', 'direct_to_template', kwargs={'template': 'home.html'}, name='home'),
 
     # Old Gallery URL
     url(r'^gallery_static/$', 'direct_to_template', {'template': 'gallery_static.html'}),
