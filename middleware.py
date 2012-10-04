e9f80cb8bc74fa9b0816e619cda727f609d37720
Venelin Stoykov <vkstoykov@gmail.com>
2012-10-05 00:16:44 +0300

    PEP8 refactoring

--- middleware.py
+++ middleware.py
@@ -4,7 +4,6 @@ from django.db import connection
 import os
 
 
-
 def terminal_width():
     """
     Function to compute the terminal width.
@@ -28,6 +27,7 @@ def terminal_width():
         width = 80
     return width
 
+
 class SQLPrintingMiddleware(object):
     """
     Middleware which prints out a list of all SQL queries done
@@ -39,22 +39,23 @@ class SQLPrintingMiddleware(object):
             or request.path_info.startswith(settings.STATIC_URL) \
             or request.path_info.startswith(settings.MEDIA_URL):
             return response
-        
+
         indentation = 2
-        print "\n\n%s\033[1;35m[SQL Queries for]\033[1;34m %s\033[0m\n" % (" "*indentation, request.path_info)
+        print "\n\n%s\033[1;35m[SQL Queries for]\033[1;34m %s\033[0m\n" % (" " * indentation, request.path_info)
         width = terminal_width()
         total_time = 0.0
         for query in connection.queries:
-            nice_sql = query['sql'].replace('"', '').replace(',',', ')
+            nice_sql = query['sql'].replace('"', '').replace(',', ', ')
             sql = "\033[1;31m[%s]\033[0m %s" % (query['time'], nice_sql)
             total_time = total_time + float(query['time'])
-            while len(sql) > width-indentation:
-                print "%s%s" % (" "*indentation, sql[:width-indentation])
-                sql = sql[width-indentation:]
-            print "%s%s\n" % (" "*indentation, sql)
-        replace_tuple = (" "*indentation, str(total_time))
+            while len(sql) > width - indentation:
+                print "%s%s" % (" " * indentation, sql[:width - indentation])
+                sql = sql[width - indentation:]
+            print "%s%s\n" % (" " * indentation, sql)
+        replace_tuple = (" " * indentation, str(total_time))
         print "%s\033[1;32m[TOTAL TIME: %s seconds]\033[0m" % replace_tuple
         return response
+
 
 class StaticServeMiddleware(object):
     """
