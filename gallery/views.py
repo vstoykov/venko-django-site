63711ed995c5061a38f8a5bbb8bf5dfcc6deb989
Venelin Stoykov <vkstoykov@gmail.com>
2013-02-17 18:58:24 +0200

    Clean the gallery.views
    Make two different views - one for galleries index and one for each gallery

--- gallery/views.py
+++ gallery/views.py
@@ -1,30 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 
 from gallery.models import Gallery
 
 
-def view_galleries(request, gallery=None):
-    if gallery:
-        gallery = get_object_or_404(Gallery, slug=gallery)
-    else:
-        galleries = Gallery.objects.exclude(pictures=None)
+def galleries(request):
+    galleries = Gallery.objects.exclude(pictures=None)
+    return render(request, 'gallery.html', {'galleries': galleries})
 
-    if request.is_ajax():
-        import json
-        if gallery:
-            json_obj = gallery.pictures.all()
-        else:
-            json_obj = galleries
-        # Prepare object for JSON
-        json_list = []
-        for obj in json_obj:
-            json_dict = {}
-            for item, value in obj.__dict__.items():
-                # Exclude special attributes (begins with '_')
-                if item[0] != '_':
-                    json_dict[item] = unicode(value)
-            json_list.append(json_dict)
-        return HttpResponse(json.dumps(json_list), content_type='text/json')
-
-    return render(request, 'gallery.html', locals())
+def gallery(request, slug):
+    gallery = get_object_or_404(Gallery, slug=slug)
+    return render(request, 'gallery.html', {'gallery': gallery})
