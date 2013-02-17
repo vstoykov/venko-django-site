0e9906ded180162ca231a55d83c24aff5f530f3d
Venelin Stoykov <vkstoykov@gmail.com>
2013-02-17 19:12:13 +0200

    Separate gallery tempalate into two different extending one common

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gallery.views',
-    url(r'^$', 'view_galleries', name='galleries'),
-    url(r'^(?P<gallery>[\w\_\-]+)/$', 'view_galleries', name='gallery'),
+    url(r'^$', 'galleries', name='galleries'),
+    url(r'^(?P<slug>[\w\_\-]+)/$', 'gallery', name='gallery'),
 )
--- gallery/views.py
+++ gallery/views.py
@@ -5,7 +5,7 @@ from gallery.models import Gallery
 
 def galleries(request):
     galleries = Gallery.objects.exclude(pictures=None)
-    return render(request, 'gallery.html', {'galleries': galleries})
+    return render(request, 'galleries.html', {'galleries': galleries})
 
 def gallery(request, slug):
     gallery = get_object_or_404(Gallery, slug=slug)
