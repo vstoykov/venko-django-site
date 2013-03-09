03b53583d56154f14ff21b4614151048cd49ea10
Venelin Stoykov <vkstoykov@gmail.com>
2013-03-09 23:37:17 +0200

    Migrate to Django 1.5

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,5 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url, patterns
 
 urlpatterns = patterns('django.contrib.flatpages.views',
     url(r'^$', 'flatpage', kwargs={'url': '/'}, name='home'),
-) + patterns('django.views.generic.simple',
-    #url(r'^$', 'direct_to_template', kwargs={'template': 'home.html'}, name='home'),
-
-    # Old Gallery URL
-    url(r'^gallery_static/$', 'direct_to_template', {'template': 'gallery_static.html'}),
 )
