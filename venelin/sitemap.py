b73d82a684f37866163202643b990e5fe73fd634
Venelin Stoykov <vkstoykov@gmail.com>
2013-08-24 16:50:16 +0300

    Improve sitemap
    - Add blog categories
    -  Show only active Galleries and published blog entries

--- venelin/sitemap.py
+++ venelin/sitemap.py
@@ -1,20 +1,30 @@
 from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap
 
-from blog.models import Entry
+from blog.models import Entry, Category
 from gallery.models import Gallery
 
-blog_dict = {
-    'queryset': Entry.objects.all(),
-    'date_field': 'modified',
-}
-
-gallery_dict = {
-    'queryset': Gallery.objects.all(),
-    'date_field': 'modified',
-}
 
 sitemaps = {
     'flatpages': FlatPageSitemap,
-    'blog': GenericSitemap(blog_dict, priority=0.9),
-    'gallery': GenericSitemap(gallery_dict, priority=0.6),
+    'blog_entries': GenericSitemap(
+        {
+            'queryset': Entry.objects.published(),
+            'date_field': 'modified',
+        },
+        priority=0.9
+    ),
+    'blog_categories': GenericSitemap(
+        {
+            'queryset': Category.objects.with_last_modified(),
+            'date_field': 'modified',
+        },
+        priority=0.8
+    ),
+    'gallery': GenericSitemap(
+        {
+            'queryset': Gallery.objects.active(),
+            'date_field': 'modified',
+        },
+        priority=0.6
+    ),
 }
