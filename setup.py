44a8adb2d12c758b914be9a09684fb2004f2112c
Venelin Stoykov <vkstoykov@gmail.com>
2013-12-03 00:14:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup
 
 setup(
     name='venelins_site',
-    version='1.0',
+    version='1.0.1',
     description='venelin.sytes.net',
     author='Venelin Stoykov',
     author_email='vkstoykov@gmail.com',
     url='http://venelin.sytes.net/',
     install_requires=[
-        'Django>=1.5.5',
+        'Django>=1.6',
         'Pillow>=2.1.0',
         'South>=0.8.2',
         'django-extensions>=1.2.0',
-        'django-tinymce>=1.5.1',
+        'django-tinymce>=1.5.2',
         'django-disqus>=0.4.1',
         'django-imagekit>=3.0.3',
         'django-admin-tools',
