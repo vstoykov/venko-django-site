22e2122fba2c4a7c770c1c61cc2b03f9b7833c50
Venelin Stoykov <vkstoykov@gmail.com>
2014-06-21 00:44:44 +0300

    Fix wsgi application files to work in OpenShift

--- venelin/wsgi.py
+++ venelin/wsgi.py
@@ -1,6 +1,7 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "venelin.settings")
+ENV = os.getenv("DJANGO_ENV") or os.getenv("OPENSHIFT_APP_NAME") and "openshift" or "dev"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "venelin.settings.%s" % ENV)
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
