f4c4b1600db69d5f1145baa14982b16f269e3fbc
Venelin Stoykov <vkstoykov@gmail.com>
2014-06-26 23:43:37 +0300

    Improve settings.openshift DB settings
    - Configure PostgreSQL only if it is installed in the gear
    - Configure CloudSQL DB if it is added to a gear

--- venelin/settings/openshift.py
+++ venelin/settings/openshift.py
@@ -1,7 +1,7 @@
 import os
 
 from .common import *
-from openshiftlibs import openshift_secure
+from openshiftlibs import openshift_secure, get_claud_db_settings
 
 BASE_DIR = os.environ['OPENSHIFT_DATA_DIR']
 
@@ -14,7 +14,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'site.db'),
     },
-    'postgres': {
+}
+DATABASES['default'] = DATABASES['sqlite']
+
+if os.getenv('OPENSHIFT_POSTGRESQL_DB_USERNAME'):
+    DATABASES['postgres'] = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['OPENSHIFT_APP_NAME'],
         'USER': os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],
@@ -22,7 +26,9 @@ DATABASES = {
         'HOST': os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
         'PORT': os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'],
     }
-}
-DATABASES['default'] = DATABASES['postgres']
+    DATABASES['default'] = DATABASES['postgres']
+
+if os.getenv('CLOUDSQL_KEY'):
+    DATABASES['cloudsql'] = get_claud_db_settings(os.environ['CLOUDSQL_KEY'])
 
 SECRET_KEY = openshift_secure('qi!k%l+n@hs8l8%)t@j2bl6_jj_x2q-g^em=i!6m17(7x1^$9r')
