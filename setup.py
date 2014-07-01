bbbdded775dd2d77b548a8c401844a4ab5d032c9
Venelin Stoykov <vkstoykov@gmail.com>
2014-07-02 00:47:51 +0300

    Update to Django1.7

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ setup(
     author_email='vkstoykov@gmail.com',
     url='http://venelin.sytes.net/',
     install_requires=[
-        'Django==1.6.6',
+        'Django==1.7c2',
         'Pillow==2.4.0',
-        'South>=0.8.4',
-        'django-extensions>=1.3.7',
+        'django-extensions>=1.3.8',
         'django-ckeditor-updated==4.4.0',
         'django-imagekit>=3.2.1',
         'django-wpadmin>=1.6.1,<1.7',
