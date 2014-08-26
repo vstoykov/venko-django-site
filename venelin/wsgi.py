147759e701c2f3a90592e1b988f0d18a9acb0ca6
Venelin Stoykov <vkstoykov@gmail.com>
2014-08-27 00:16:19 +0300

    Update wsgi.py file
    Use `get_wsgi_application` function instead of manually using 
    `django.core.handlers.wsgi.WSGIHandler`.

--- venelin/wsgi.py
+++ venelin/wsgi.py
@@ -1,9 +1,15 @@
+"""
+WSGI config for venelin.sytes.net project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
 import os
 
 ENV = os.getenv("DJANGO_ENV") or os.getenv("OPENSHIFT_APP_NAME") and "openshift" or "dev"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "venelin.settings.%s" % ENV)
 
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
