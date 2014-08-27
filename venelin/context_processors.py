595bc015acfd7e4083581d1b989d05d5cef59e3e
Venelin Stoykov <vkstoykov@gmail.com>
2014-08-28 00:19:34 +0300

    Put the logic for when to show google analytics code in context processor

--- venelin/context_processors.py
+++ venelin/context_processors.py
@@ -2,6 +2,8 @@ from django.conf import settings
 
 
 def google_analytics(request):
+    if settings.DEBUG or request.user.is_staff:
+        return {}
     return {
         'GOOGLE_ANALYTICS_CODE': getattr(settings, 'GOOGLE_ANALYTICS_CODE', None),
         'GOOGLE_ANALYTICS_DOMAIN': getattr(settings, 'GOOGLE_ANALYTICS_DOMAIN', request.get_host()),
