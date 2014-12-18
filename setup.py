fc6708ccda6fdf2e98f8e2d0b16636d7789a335d
Venelin Stoykov <vkstoykov@gmail.com>
2014-12-18 02:51:19 +0200

    Update some packages

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ from setuptools import setup
 
 setup(
     name='venelins_site',
-    version='1.2.1',
+    version='1.2.2',
     description='venelin.sytes.net',
     author='Venelin Stoykov',
     author_email='vkstoykov@gmail.com',
     url='http://venelin.sytes.net/',
     install_requires=[
-        'Django==1.7',
-        'Pillow==2.4.0',
-        'django-extensions>=1.3.8',
+        'Django==1.7.1',
+        'Pillow==2.6.1',
+        'django-extensions>=1.4.9',
         'django-ckeditor-updated==4.4.0',
         'django-imagekit>=3.2.1',
-        'django-wpadmin>=1.6.1,<1.7',
+        'django-wpadmin==1.7.0',
         'pygments',
         'appenlight-client==0.6.9',
         'psycopg2',
+        'django_bmemcached==0.2.2',
     ],
 )
