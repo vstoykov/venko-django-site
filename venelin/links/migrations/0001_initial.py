32cc71a311a55ed8bfde6adec53338bcf044ba8d
Venelin Stoykov <vkstoykov@gmail.com>
2015-04-22 00:18:04 +0300

    Add missing initial migration for links application

---
+++ venelin/links/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('title', models.CharField(verbose_name='title', db_index=True, max_length=255)),
+            ],
+            options={
+                'verbose_name': 'category',
+                'verbose_name_plural': 'categories',
+                'ordering': ('title',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Link',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('title', models.CharField(verbose_name='title', max_length=255)),
+                ('url', models.CharField(verbose_name='URL', max_length=255)),
+                ('category', models.ForeignKey(to='links.Category', related_name='links', verbose_name='category')),
+            ],
+            options={
+                'verbose_name': 'link',
+                'verbose_name_plural': 'links',
+                'ordering': ('category',),
+            },
+        ),
+    ]
