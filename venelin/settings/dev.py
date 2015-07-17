c60ff37a7460724d18381053f2f623692482c554
Venelin Stoykov <vkstoykov@gmail.com>
2015-07-18 02:45:38 +0300

    Configure django-debug-toolbar to use template timing panel if available

--- venelin/settings/dev.py
+++ venelin/settings/dev.py
@@ -34,6 +34,28 @@ try:
 except ImportError:
     pass
 else:
-    INSTALLED_APPS += (
-        'debug_toolbar',
-    )
+    INSTALLED_APPS += 'debug_toolbar',
+
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ]
+    try:
+        __import__('template_timings_panel')
+    except ImportError:
+        pass
+    else:
+        INSTALLED_APPS += 'template_timings_panel',
+        DEBUG_TOOLBAR_PANELS.append(
+            'template_timings_panel.panels.TemplateTimings.TemplateTimings'
+        )
