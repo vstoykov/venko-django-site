caab7b15ad976f0d3e874e0288e4aaffd4ccd768
Venelin Stoykov <vkstoykov@gmail.com>
2018-07-03 23:33:16 +0300

    Make it possible to run tests with setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import sys
 from setuptools import setup
+
+needs_pytest = bool({'pytest', 'test', 'ptr'}.intersection(sys.argv))
 
 setup(
     name='venelin-site',
@@ -9,6 +12,7 @@ setup(
     author='Venelin Stoykov',
     author_email='vkstoykov@gmail.com',
     url='http://venelin.sytes.net/',
+    setup_requires=['pytest-runner'] if needs_pytest else [],
     install_requires=[
         'Django>=1.11,<3',
         'Pillow>=4.3.0',
@@ -21,4 +25,5 @@ setup(
         'appenlight-client>=0.6.25',
         'psycopg2',
     ],
+    tests_require=['pytest-django'],
 )
