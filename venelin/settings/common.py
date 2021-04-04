d1e495cc9b6b9baa85567b80a86f22936b426596
Venelin Stoykov <vkstoykov@gmail.com>
2021-04-05 00:32:39 +0300

    There is no need of STATICFILES_DIRS

--- venelin/settings/common.py
+++ venelin/settings/common.py
@@ -57,9 +57,6 @@ USE_L10N = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'wsgi', 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'venelin', 'static'),
-)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
