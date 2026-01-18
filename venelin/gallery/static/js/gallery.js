15b97f8536c3aa3685943550442b05b4af279131
Venelin Stoykov <vkstoykov@gmail.com>
2026-01-18 23:44:22 +0200

    Use lightgallery from CDN

--- venelin/gallery/static/js/gallery.js
+++ venelin/gallery/static/js/gallery.js
@@ -1,65 +1,154 @@
-var galleries_obj;
-var gallery_json = '/static/js/gallery.json'
+/*globals jQuery, window */
+/*jslint plusplus: true, nomen: true, regexp: true, vars: true */
 
-function galleryShow(galleryName) {
-  var gallery = galleries_obj[galleryName]
-  var pictures = gallery.pictures;
-  $('#gallery_list').html('');
-  var picture;
-  for (var item in pictures) {
-    picture = pictures[item];
-    $('#gallery_list').append('<li><div class="thumb-holder"><a href="'+picture.src+'"><img src="'+picture.thumb.src+'" width="'+picture.thumb.width+'" height="'+picture.thumb.height+'" alt="" class="shadow" /></a></div><!--p>'+picture.title+'</p--></li>');
-    //$('#gallery_list').append('<li><a href="'+picture.src+'">'+picture.thumb.outerHTML+'</a><p>'+picture.title+'</p></li>');
-  }
-  $('#gallery_list a').lightBox();
-  $('#galleries_list').hide()
-  $('#gallery_list').fadeIn('slow', function(){$('.backToGalleries').css('visibility', 'visible');});
-}
+(function ($) {
+    "use strict";
 
-function listGalleries(galleries) {
-  if (galleries == undefined) galleries = galleries_obj;
-  $('.backToGalleries').css('visibility', 'hidden');
-  $('#galleries_list').html('');
-  var gallery_obj;
-  for (var gallery in galleries) {
-    gallery_obj = galleries[gallery];
-    $('#galleries_list').append('<li><a href="javascript:void(0)" id="gallery_'+gallery+'" name="'+gallery+'"><img src="/static/img/gallery-folder.png" class="shadow" /><p>'+gallery_obj.title+'</p></a></li>');
-    $('#gallery_'+gallery).click(function(){
-      var gallery = $(this).attr('name');
-      galleryShow(gallery);
-    });
-  }
-  $('#gallery_list').hide()
-  $('#galleries_list').fadeIn('slow');
-}
-
-function loadGalleryData() {
-  $.getJSON(gallery_json, function(data) {
-    //console.log(data);
-    var gallery_obj, pictures;
-    var picture, g_location, pic_location, thumb_pic;
-    for(var gallery in data) {
-        gallery_obj = data[gallery];
-        pictures = gallery_obj.pictures
-        gallery_obj.pictures_obj = {}
-        for(var item in pictures) {
-            picture = pictures[item];
-            g_location = '/static/gallery/'+gallery+'/';
-            pic_location = g_location + picture;
-            thumb_pic = new Image();
-            thumb_pic.src = g_location + 'thumbs/' + picture;;
-            gallery_obj.pictures_obj[item] = {'src':pic_location, 'thumb':thumb_pic, 'title': picture.replace(/_/g, ' ')}
+    function Gallery (data) {
+      this.index = data.index;
+      this.slug = data.slug;
+      this.url = data.url;
+      this.title = data.title;
+      this.$element = null;
+      this.pictures = $.map(data.pictures, function (name) {
+        return {
+          src: this.url + name,
+          title: name.replace(/[_\-]/g, ' ').split('.')[0],
+          thumb: {
+            src: this.url + 'thumbs/' + name
+          }
         };
-        gallery_obj.pictures = gallery_obj.pictures_obj
-        delete gallery_obj.pictures_obj
+      }.bind(this));
+    };
+
+    Gallery.prototype.getThumbnailUrl = function () {
+      return this.pictures[0].thumb.src;
     }
 
-    galleries_obj = data;
+    Gallery.prototype.getElement = function () {
+        if (!this.$element) {
+          this.$element = this.render().hide().insertAfter(this.index.$element);
+          $('a', this.$element).venobox();
+        }
+        return this.$element;
+    };
 
-    listGalleries(data)
-  });
-}
+    Gallery.prototype.render = function () {
+      return $('<div>', {
+        'class': 'card-columns',
+        'id': this.url,
+      }).append(
+        $.map(this.pictures, this.renderPicture.bind(this))
+      );
+    }
 
-$(document).ready(function(){
-  if (galleries_obj == undefined) loadGalleryData();
-});
+    Gallery.prototype.renderPicture = function (picture) {
+      return $('<figure>').append(
+        $('<a>', {
+          'class': "card img-fluid",
+          'href': picture.src,
+          'title': picture.title,
+          'data-gall': this.slug,
+        }).append(
+          $('<img>', {
+            'src': picture.thumb.src,
+            'alt': picture.title,
+            'class': 'card-img-top'
+          })
+        )
+      );
+    }
+
+    function Galleries (options) {
+      this.options = options || {};
+      this.items = [];
+      this.$element = $(this.options.element);
+      this.$title = $(this.options.title);
+      this.listIndexTitle = this.$title.text();
+      this.$breadcrumb = $(this.options.breadcrumb);
+      this.$backButtons = $(this.options.backButtons);
+
+      this.$backButtons.on('click', function(e) {
+        this.listIndex()
+        e.preventDefault();
+      }.bind(this));
+
+      this.$element.on('click', 'a[data-gallery]', function (e) {
+        var $card = $(e.currentTarget).closest('.card');
+        this.showGallery($card.data('gallery'));
+        e.preventDefault();
+      }.bind(this));
+    };
+
+    Galleries.prototype.loadData = function () {
+      $.ajax({
+        url: this.options.index,
+        context: this,
+        dataType: 'json',
+      }).done(function (response) {
+        this.items = $.map(response, function (data, slug) {
+          return new Gallery({
+            index: this,
+            slug: slug,
+            url: this.options.url + slug + '/',
+            title: data.title,
+            pictures: data.pictures
+          });
+        }.bind(this));
+        this.render();
+        this.listIndex();
+      });
+    };
+
+    Galleries.prototype.render = function () {
+      this.$element.html('');
+      var elements = $.map(this.items, this.renderGaleryCard);
+      this.$element.append(elements);
+    }
+
+    Galleries.prototype.renderGaleryCard = function(gallery) {
+      var url = '#' + gallery.url;
+      var $card = $('<div class="card">').append(
+        $('<a>', {
+          'href': url,
+          'data-gallery': gallery.slug
+        }).append(
+          $('<img>', {
+            'src': gallery.getThumbnailUrl(),
+            'class': 'card-img-top',
+          })
+        )
+      ).append(
+        $('<div class="card-body text-center">').append(
+          $('<a>', {
+            'href': url,
+            'class': 'h4',
+            'data-gallery': gallery.slug
+          }).text(gallery.title)
+        )
+      ).data('gallery', gallery);
+      return $card;
+    }
+
+    Galleries.prototype.showGallery = function(gallery) {
+      var $el = gallery.getElement();
+      this.$element.hide();
+      $el.fadeIn('slow');
+      this.$title.text(gallery.title);
+      $('.breadcrumb-item.active', this.$breadcrumb).text(gallery.title);
+      this.$breadcrumb.css('visibility', 'visible');
+      this.$backButtons.css('visibility', 'visible');
+    }
+    Galleries.prototype.listIndex = function (e) {
+      var loadedGalleries = $.map(this.items, function(item) {
+        return item.$element ? item.$element.get(0) : null;
+      }).filter(Boolean);
+      $(loadedGalleries).hide();
+      this.$element.fadeIn('slow');
+      this.$title.text(this.listIndexTitle);
+      this.$breadcrumb.css('visibility', 'hidden');
+      this.$backButtons.css('visibility', 'hidden');
+    };
+
+    window.Galleries = Galleries;
+})(jQuery);
