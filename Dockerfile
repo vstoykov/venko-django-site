vstoykov/venko-django-site

commit 18ceb754643d7241d08a5638c4ecd670c9dacfa5

chore: multistage build
Use multi stag build to reduce size of the production image

Dockerfile
@@ -1,17 +1,16 @@
-FROM python:3.14-slim
+FROM python:3.14 as build
 
 RUN set -eux; \
     export DEBIAN_FRONTEND=noninteractive; \
     apt-get update; \
     apt-get install --no-install-recommends --no-install-suggests --yes gettext; \
-    apt-get remove --purge --auto-remove --yes; \
-    rm -rf /var/lib/apt/lists/*;
+    apt-get clean;
 
 COPY pyproject.toml uv.lock /app/
 WORKDIR /app/
 
 RUN --mount=type=cache,target=/root/.cache set -eux; \
-    pip install --root-user-action=ignore --no-cache-dir uv; \
+    pip install --root-user-action=ignore uv; \
     uv sync --frozen --extra=uwsgi --extra=postgres --no-dev --link-mode=copy; \
     pip uninstall --root-user-action=ignore --yes uv;
 
@@ -30,4 +29,16 @@ RUN set -eux; \
     chown www-data:www-data /app/www/media; \
     chmod g+w /app/www/media;
 
-CMD [ ".venv/bin/uwsgi", "--master", "--ini=uwsgi.ini", "--http=0.0.0.0:8000", "--uid=www-data", "--gid=www-data", "--env=HOME=/app"]
+
+FROM python:3.14-slim as production
+
+RUN set -eux; \
+    export DEBIAN_FRONTEND=noninteractive; \
+    apt-get update; \
+    apt-get install --no-install-recommends --no-install-suggests --yes libxml2;
+
+COPY --from=build /app/ /app/
+WORKDIR /app/
+ENV DJANGO_ENV=production
+
+CMD [ "/app/.venv/bin/uwsgi", "--master", "--ini=/app/uwsgi.ini", "--http=0.0.0.0:8000", "--uid=www-data", "--gid=www-data", "--env=HOME=/app"]

commit 33ae3d8bc32d3b367377cb106d15d7d1ca9be91a

fix: add mising media-types to production image
Regression from 18ceb754643d7241d08a5638c4ecd670c9dacfa5

Dockerfile
@@ -35,7 +35,8 @@ FROM python:3.14-slim as production
 RUN set -eux; \
     export DEBIAN_FRONTEND=noninteractive; \
     apt-get update; \
-    apt-get install --no-install-recommends --no-install-suggests --yes libxml2;
+    apt-get install --no-install-recommends --no-install-suggests --yes libxml2 media-types; \
+    apt-get clean;
 
 COPY --from=build /app/ /app/
 WORKDIR /app/